65d0be2dfc7a1164f44882c7e591e35dda4f2d2b
uswebk <usweb111@gmail.com>
2020-11-22 22:11:50 +0900

    first commit

---
+++ src/laravelapp/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('hello/{msg?}', function($msg='no') {
+    $html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+</head>
+<style>
+body {font-size:16pt; color:#999}
+h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+</style>
+<body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+    return $html;
+});
